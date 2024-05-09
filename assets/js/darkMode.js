e8a23a2265045fc2ca91a75e0ecd584e899ea8b0
CrisSal2 <salgado.chris.m@gmail.com>
2024-05-08 20:46:34 -0700

    made buttons for home page

---
+++ assets/js/darkMode.js
@@ -0,0 +1,13 @@
+const icon = document.getElementById("icon");
+
+icon.onclick = function() {
+
+    document.body.classList.toggle("darkTheme");
+
+    if (document.body.classList.contains("darkTheme")) {
+        icon.src ="./assets/img/sun.png"
+    } else {
+        icon.src = "./assets/img/moon.png"
+    }
+
+}
